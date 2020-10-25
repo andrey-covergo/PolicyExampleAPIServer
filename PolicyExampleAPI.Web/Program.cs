37a15972442db821a95a93ffaddd916506915f43
Andrey Leskov <andrey@covergo.com>
2020-10-25 21:51:00 +0800

    Add autogenerated controllers layer and project for server part

--- PolicyExampleAPI.Web/Program.cs
+++ PolicyExampleAPI.Web/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace PolicyExampleAPIServer
+namespace PolicyExampleAPI.Web
 {
     public class Program
     {
---
+++ PolicyExampleAPIServer.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace PolicyExampleAPIServer.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
+
+
+//dotnet tool install --global NSwag.ConsoleCore
